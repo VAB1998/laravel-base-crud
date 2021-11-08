267dbe8ccee1d270dad43099d39dd1effb9d08ce
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-08 16:23:34 +0100

    .

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -46,7 +46,7 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic) //
     {
         //
     }
