1f4aba4bcf835b39cf491e46755bc0d689fb3ed9
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-09 21:20:26 +0100

    Added SoftDeletes

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     public $timestamps = false;
+    use SoftDeletes;
     protected $fillable = ['title', 'description', 'thumb', 'writer'];
 }
